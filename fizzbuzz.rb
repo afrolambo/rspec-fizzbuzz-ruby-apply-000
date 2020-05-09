30bfd8bfc5ff3df7b88cbbe219005afec3af929a
Stephen C Galvan <stephennoahgalvan@gmail.com>
2020-05-09 00:36:30 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -5,7 +5,7 @@ def fizzbuzz(int)
     return "Fizz"
   elsif int % 5 == 0
     return "Buzz"
-  elsif "Buzz" && "Fizz"
+  elsif int == "Buzz" && "Fizz"
     return "FizzBuzz"
   else
     return nil
